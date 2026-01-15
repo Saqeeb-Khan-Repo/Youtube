489228af443fa5b62a6d3d162beac75947e6fc29
Mr.Shahid <shahid.gce@gmail.com>
2026-01-15 09:17:46 +0530

    /

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 // vite.config.js
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || "/Youtube", // safe: env might not have it
+    base: process.env.VITE_BASE_PATH || "/Youtube", // safe: env might not have it
   };
 });
